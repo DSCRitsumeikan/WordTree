d4fc0c25173e2f5b066c7494cdac81cd75cbd1f2
kenboo0426 <kenshin.kenboo@gmail.com>
2023-03-10 15:19:39 +0900

    set up gorm

---
+++ api/infrastructure/gorm.go
@@ -0,0 +1,59 @@
+package infrastructure
+
+import (
+	"WordTree/config"
+	"fmt"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+	db := InitGorm()
+	return db
+}
+
+func InitGorm() *gorm.DB {
+	driver := config.Mysql{
+		Host:     config.GetEnvWithDefault("DB_HOST", "db"),
+		Port:     config.GetEnvWithDefault("DB_PORT", "3306"),
+		UserName: config.GetEnvWithDefault("DB_USER", "wordtree_user"),
+		Password: config.GetEnvWithDefault("DB_PASSWORD", "password"),
+		DBName:   config.GetEnvWithDefault("DB_NAME", "wordtree_dev"),
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", driver.UserName, driver.Password, driver.Host, driver.Port, driver.DBName)
+	db := connectDB(dsn)
+	autoMigrate(db)
+	return db
+}
+
+func connectDB(dsn string) *gorm.DB {
+	db, err := openDBWithTimeLimit(dsn, 5)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func openDBWithTimeLimit(dsn string, count int) (*gorm.DB, error) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		if count == 0 {
+			return nil, fmt.Errorf("retry count over")
+		}
+		time.Sleep(time.Second)
+		count--
+		return openDBWithTimeLimit(dsn, count)
+	}
+	return db, nil
+}
+
+func autoMigrate(db *gorm.DB) {
+	err := db.AutoMigrate(
+	// new()
+	)
+	if err != nil {
+		panic(err)
+	}
+}
